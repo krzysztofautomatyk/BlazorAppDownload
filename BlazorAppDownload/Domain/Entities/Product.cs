0bd08493f8675ea479e916359424b96302a9b2c8
Krzysztof Wozniczak <Krzysztof@DESKTOP-ROJJHH1>
2024-11-06 15:43:56 +0100

    Bug

--- BlazorAppDownload/Domain/Entities/Product.cs
+++ BlazorAppDownload/Domain/Entities/Product.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using CsvHelper.Configuration.Attributes;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace BlazorAppDownload.Domain.Entities;
@@ -8,16 +9,20 @@ public class Product
     [Key]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("id")]
+    [Ignore] // CSVHELPER
     public int Id { get; set; }
 
     [Column("product_code")]
     [MaxLength(100)]
-    public string? Name { get; set; }
+    [Name("product_code")]
+    public string? ProductCode { get; set; }
 
     [Column("product_name")]
     [MaxLength(20)]
+    [Name("product_name")]
     public string? ProductName { get; set; }
 
     [Column("price", TypeName = "decimal(7,2)")]
+    [Name("price")]
     public decimal Price { get; set; }
 }
