296e4f2c5a7a2ce31d72e9763df39f591e54759f
Krzysztof Wozniczak <Krzysztof@DESKTOP-ROJJHH1>
2024-11-06 17:18:27 +0100

    USE CHROME :)

--- BlazorAppDownload/Program.cs
+++ BlazorAppDownload/Program.cs
@@ -10,7 +10,19 @@ builder.Services.AddRazorComponents()
 
 // SQL
 builder.Services.AddDbContext<AppDbContext>(opt =>
-    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));    
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection")));
+
+var logger = builder.Services.BuildServiceProvider().GetRequiredService<ILogger<Program>>();
+AppDomain.CurrentDomain.UnhandledException += (sender, args) =>
+{
+    logger.LogError(args.ExceptionObject as Exception, "Unhandled exception");
+};
+
+builder.Services.AddServerSideBlazor().AddCircuitOptions(options =>
+{
+    options.DisconnectedCircuitMaxRetained = 100;
+    options.DisconnectedCircuitRetentionPeriod = TimeSpan.FromMinutes(3);
+});
 
 
 var app = builder.Build();
@@ -22,6 +34,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+app.UseWebSockets();
 
 app.UseHttpsRedirection();
 
